6095e177b5935e1ebd65d5f3109968c39ee82248
mohamed_ezz <mohamed_abdelhady1@outlook.com>
2023-02-16 17:38:21 -0800

    l

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 /**
@@ -7,7 +8,7 @@
  */
 int main(void)
 {
-	write(STDOUT_FILEN, "and that piece of art is useful\"
-			- Dora Korpar, 2015-10-19\n", 59);
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
