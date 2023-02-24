c389047a8665ac1b505a5256dc0431f2c741a7d1
mohamed ahmed ezz <120762502+mohamed-ezz19@users.noreply.github.com>
2023-02-24 02:10:51 +0200

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,18 +4,25 @@
  * print_triangle - prints a triangle, followed by a new line
  * @size: size of the triangle
  */
-void print_triangle(int size) {
-  if (size <= 0) {
+void print_triangle(int size)
+{
+  if (size <= 0)
+  {
     _putchar('\n');
-  } else {
+  } 
+  else 
+  {
     int i, j;
 
-    for (i = 1; i <= size; i++) {
-      for (j = i; j < size; j++) {
+    for (i = 1; i <= size; i++)
+    {
+      for (j = i; j < size; j++)
+      {
         _putchar(' ');
       }
 
-      for (j = 1; j <= i; j++) {
+      for (j = 1; j <= i; j++) 
+      {
         _putchar('#');
       }
 
